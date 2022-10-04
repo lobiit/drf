abcfca18f822403199cd66c0afa1bd1a80676600
Tumbo Lobi John <johnlobiit@gmail.com>
2022-10-04 16:01:29 +0300

    creating get request

---
+++ py_client/basic.py
@@ -0,0 +1,7 @@
+import requests
+
+# endpoint = "http://httpbin.org/anything"
+endpoint = 'http://127.0.0.1:8000/api/'
+
+get_response = requests.get(endpoint, params={"awe": 123}, json={'query': 'Hello world'})
+print(get_response.json())
