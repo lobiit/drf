d8f088a4f4429fd1f013b89bd71eeaf4d4a1889f
Tumbo Lobi John <johnlobiit@gmail.com>
2022-10-05 10:58:25 +0300

    rest framework view and response

--- api/views.py
+++ api/views.py
@@ -1,13 +1,16 @@
-from django.http import JsonResponse
+from django.forms import model_to_dict
+from django.http import JsonResponse, HttpResponse
 import json
 from products.models import Product
 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+
+@api_view(["GET", "POST"])
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by('?').first()
     data = {}
     if model_data:
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
-    return JsonResponse(data)
+        data = model_to_dict(model_data, fields=['id', 'title'])
+    return Response(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -5,3 +5,4 @@ endpoint = 'http://127.0.0.1:8000/api/'
 
 get_response = requests.get(endpoint, params={"awe": 123}, json={'query': 'Hello world'})
 print(get_response.json())
+
